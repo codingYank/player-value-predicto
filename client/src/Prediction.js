b467ad8470c334de2eef1e501432144d72571bfc
Tyler-Schmidt-Wissota <tyler.schmidt@wissotagroup.com>
2023-06-07 12:35:16 -0400

    new visual

--- client/src/Prediction.js
+++ client/src/Prediction.js
@@ -2,6 +2,7 @@ import React from "react"
 import ageValuePlot from "./assets/age-value-plot.png"
 import gamesGoalsPlot from "./assets/games-goals-plot.png"
 import ppgValuePlot from "./assets/ppg-value-plot.png"
+import ageMaxValueBar from "./assets/age-max-value-bar.png"
 
 const Prediction = ({ value, data }) => {
   console.log(value)
@@ -22,6 +23,10 @@ const Prediction = ({ value, data }) => {
             and their value.
           </p>
           <img src={ageValuePlot} alt="Age and value plot"></img>
+        </div>
+        <div className="data-visual-item">
+          <p>This bar graph shows the most valuable player at each age.</p>
+          <img src={ageMaxValueBar} alt="Max value at each age bar graph"></img>
         </div>
         <div className="data-visual-item">
           <p>
